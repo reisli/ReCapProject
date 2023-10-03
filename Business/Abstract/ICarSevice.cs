b670a807acf9789c04b85128cd5790e81f8bbd96
Ulvi Reisli <reisli007@gmail.com>
2023-10-03 15:32:06 +0400

    Next Release

--- Business/Abstract/ICarSevice.cs
+++ Business/Abstract/ICarSevice.cs
@@ -2,7 +2,6 @@
 using Entities.Concrete;
 using Entities.DTOs;
 using System;
-
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ WebAPI/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpPost]
+        public IActionResult Add(Customer customer)
+        {
+            var result = _customerService.Add(customer);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
+    }
+}
